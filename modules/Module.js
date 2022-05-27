5722e54fead77e5ae2b0bcd01c07d497a401cb6f
micty <mictydeng@qq.com>
2022-05-27 17:34:24 +0800

    2.1.1

--- modules/Module.js
+++ modules/Module.js
@@ -29,12 +29,31 @@ module.exports = {
         let lines = Lines.split(content);
         let startNo = 0;    //下次搜索的起始行号
 
+        lines = lines.filter((line) => {
+            line = line.trimLeft();
+
+            if (line.startsWith('//') ||
+                line.startsWith('/*') ||
+                line.startsWith('*')) {
+                return false;
+            }
+
+            return true;
+        });
+
+
         list = list.map((item, index) => {
             item = item.split('\n\r').join('');
             item = item.split('\n').join('');
             item = item.split('\r').join('');
 
             let no = Lines.getIndex(lines, item, startNo);  //行号。
+
+            //没有找到该行，可能是给注释掉了。
+            if (no < 0) {
+                return null;
+            }
+
             let line = lines[no];                           //整一行的 html。
 
 
